cd0ef0fb59353f7914edc28cabbfd284b3166942
Roman Jouhannet <r.jouhannet@f5.com>
2021-03-07 12:34:06 -0800

    improve restart script

--- restart.sh
+++ restart.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # Uncomment set command below for code debugging bash
 #set -x
-
+echo -e "\nstop carwatch..."
 killall python3
+echo -e "\nwait 5 secs..."
 sleep 5
+echo "\ncarwatch starting..."
 nohup python3 carwatch.py 2>&1 &> carwatch.log &
+echo -e "\nwait 5 secs..."
 sleep 5
-tail -15 carwatch.log
+echo -e "\nlogs..."
+tail -20 carwatch.log
