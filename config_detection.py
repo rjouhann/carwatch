29460aa80b9fb9689b556a7f0e4bd7b5347e7e10
Roman Jouhannet <r.jouhannet@f5.com>
2021-03-08 17:07:39 -0800

    tune detection settings

--- config_detection.py
+++ config_detection.py
@@ -9,10 +9,10 @@ debug = False
 
 # detection car limits
 limit_detected = 20 # recommended below 20
-limit_good = 400 # this can be tweek recommended between 300-500
-limit_bad = 800 # this can be tweek recommended between 700-1200, at least 2x limit_good
+limit_good = 270 # this can be tweek recommended between 300-500
+limit_bad = 1000 # this can be tweek recommended between 700-1200, at least 2x limit_good
 # when to take the screenshot 
-good_car_screenshot = 450 # must be less than delay
+good_car_screenshot = 300 # must be less than delay
 # time to wait before resetting the detection loop
 delay = 1500 # must be less than delay
 # detection car settings
